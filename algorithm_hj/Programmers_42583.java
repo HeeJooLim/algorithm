367c8a8a3f3bbcd4b8512256cd87de6c7d8e8240
HeeJooLim <hhaanbb@naver.com>
2020-11-29 22:20:05 +0900

    study

--- algorithm_hj/Programmers_42583.java
+++ algorithm_hj/Programmers_42583.java
@@ -4,6 +4,10 @@ package algorithm_hj;
 import java.util.Queue;
 import java.util.LinkedList;
 
+/**
+ * 다리를 지나는 트럭
+ * https://programmers.co.kr/learn/courses/30/lessons/42583
+ */
 public class Programmers_42583 {
 
 
@@ -13,40 +17,35 @@ public class Programmers_42583 {
 		System.out.println(solution(2, 10, a));
 	}
 	
-	static int solution(int bridge_length, int weight, int[] truck_weights) {
-		int answer = 0;
-			
-		Queue<Integer> queue = new LinkedList<>();
-		
-		int sum = 0;
-		for(int t : truck_weights) {
-			
-			while(true) {
-				if(queue.isEmpty()) {
-					queue.add(t);
-					sum += t;
-					answer++;
-					break;
-				} else if(queue.size() == bridge_length){
-					sum -= queue.poll();
-				} else {
-					if(sum + t > weight) {
-						queue.add(0);
-						answer++;
-					} else {
-						queue.add(t);
-						sum += t;
-						answer++;
-						break;
-					}
-				}
-			}
-			
-		}
-	
-		
-		return answer + bridge_length;
-	}
-		
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int w = 0;
+        for(int i = 0; i < truck_weights.length; i++) {
+        	int truck = truck_weights[i];
+        	while(true) {
+	        	if(queue.isEmpty()) {
+	        		w += truck;
+	        		queue.add(truck);
+	        		answer++;
+	        		break;
+	        	} else if(queue.size() == bridge_length){
+	        		w -= queue.poll();
+	        	} else {
+	        		if(w + truck <= weight) {
+	        			w += truck;
+	        			queue.add(truck);
+	        			answer++;
+	        			break;
+	        		} else {
+	        			queue.add(0);
+	        			answer++;
+	        		}
+	        	}
+        	}
+        }
+        return answer + bridge_length;
+    }
 
+		
 }
